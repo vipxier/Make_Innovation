3239bffa36b52b8078aaa7e4007b1fd06c125c58
tengfei li <tengfei.li@cerner.com>
2019-09-20 10:26:07 -0500

    Update documentation of the LSC

--- Algorithems/src/Dyamic_Programming/LongestCommonString.java
+++ Algorithems/src/Dyamic_Programming/LongestCommonString.java
@@ -8,17 +8,14 @@ import java.util.Set;
  * @author LT028861
  *
  */
+/**
+ * @author LT028861
+ *
+ */
 public class LongestCommonString
 {
-
     /**
-     * This is the recursion solution of LSC.
-     * 
-     * @param str1
-     * @param str2
-     * @param len1
-     * @param len2
-     * @return
+     * Recurrsion Solution
      */
     private static int LSC(String str1, String str2, int len1, int len2)
     {
@@ -35,6 +32,9 @@ public class LongestCommonString
         return Math.max(LSC(str1, str2, len1 - 1, len2), LSC(str1, str2, len1, len2 - 1));
     }
 
+    /**
+     * DP solution
+     */
     private static int LSC_DP(String str1, String str2)
     {
         int[][] record = new int[str1.length() + 1][str2.length() + 1];
@@ -56,6 +56,9 @@ public class LongestCommonString
         return record[str1.length()][str2.length()];
     }
 
+    /**
+     * Return the result string array for latter processing.
+     */
     public static int[][] LSCResult(String str1, String str2)
     {
         int len1 = str1.length();
@@ -79,11 +82,14 @@ public class LongestCommonString
         return result;
     }
 
+    /**
+     * Get The longest Common string iteself;
+     */
     public static String getLCSString(String str1, String str2)
     {
         int len1 = str1.length();
         int len2 = str2.length();
-        
+
         String[][] result = new String[len1 + 1][len2 + 1];
         for (int i = 0; i <= len1; i++)
         {
@@ -114,12 +120,15 @@ public class LongestCommonString
         }
         return result[len1][len2];
     }
-    
+
+    /**
+     * Get all the common strings.
+     */
     public static Set<String> getAllLCSStrings(String str1, String str2)
     {
         int len1 = str1.length();
         int len2 = str2.length();
-        
+
         String[][] result = new String[len1 + 1][len2 + 1];
         for (int i = 0; i <= len1; i++)
         {
@@ -148,29 +157,27 @@ public class LongestCommonString
                 }
             }
         }
-        
+
         Set<String> resultSet = new HashSet<String>();
         int lscLength = result[len1][len2].length();
-        
-        for(int i = len1; i >= 1; i --)
+
+        for (int i = len1; i >= 1; i--)
         {
-            for(int j= len2; j >= 1; j--)
+            for (int j = len2; j >= 1; j--)
             {
-                if(lscLength <= result[i][j].length())
+                if (lscLength <= result[i][j].length())
                     resultSet.add(result[i][j]);
             }
         }
         return resultSet;
     }
-    
-    
 
     public static void main(String[] args)
     {
         String X = "ABCBDABCCAASCCASDA", Y = "BDCABACCSSCCAASDASD";
         System.out.println(LSC_DP(X, Y));
         System.out.println(LSC(X, Y, X.length(), Y.length()));
-        for(String str: getAllLCSStrings(X,Y))
+        for (String str : getAllLCSStrings(X, Y))
         {
             System.out.print(" " + str);
         }
