1d9fd7baaa2003bc5d1f453e96cc873484be292e
tengfei li <tengfei.li@cerner.com>
2019-10-22 22:30:24 -0500

    commit for more changes

---
+++ Lee/src/utils/TreeNode.java
@@ -0,0 +1,12 @@
+package utils;
+
+public class TreeNode
+{
+    int data;
+    TreeNode left, right;
+    
+    public TreeNode(int value) {
+        this.data = value;
+        this.left = this.right = null;
+    }
+}
