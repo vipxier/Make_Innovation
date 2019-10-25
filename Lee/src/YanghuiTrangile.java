ab74e484124f96dcaa2b2439ac0088b79d143f7f
tengfei li <tengfei.li@cerner.com>
2019-10-25 10:31:05 -0500

    YangTriangle

--- Lee/src/YanghuiTrangile.java
+++ Lee/src/YanghuiTrangile.java
@@ -3,7 +3,7 @@ import java.util.List;
 
 public class YanghuiTrangile
 {
-public List<List<Integer>> generate(int numRows) {
+public static  List<List<Integer>> generate(int numRows) {
        List<List<Integer>> result = new ArrayList<List<Integer>>();
        
        if(numRows == 0)
@@ -13,30 +13,35 @@ public List<List<Integer>> generate(int numRows) {
        
        List<Integer> list = new ArrayList<Integer>();
        list.add(1);
-       result.add(list);
+       result.add(new ArrayList<Integer>(list));
        
-      for(int i = 1; i <= numRows;i ++)
+      for(int i = 1; i < numRows;i ++)
       {
           list.clear();
           List<Integer> prevList = result.get(i - 1);
-          for(int j = 0; j < numRows; j ++)
+          for(int j = 0; j <= i; j ++)
           {
               if(j == 0)
               {
                   list.add(1);
               }
-              else if(j == numRows - 1)
+              else if(j == i)
               {
                   list.add(1);
               }
               else
               {
-                  list.add(prevList.get(i-1) + prevList.get(i));
+                  list.add(prevList.get(j - 1) + prevList.get(j));
               }
           }
           result.add(new ArrayList<Integer>(list));
       }
        
        return result;
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println(generate(10));
     }
 }
